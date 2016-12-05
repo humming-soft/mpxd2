10f48df95eb52a9951b4d8a70bdedef984ad9a9c
unknown <sebin@HS-PC07.Office.Hummingsoft.com.my>
2016-12-05 16:49:15 +0800

    Signed-off-by: unknown <sebin@HS-PC07.Office.Hummingsoft.com.my>

---
+++ application/views/module/gis_accessories.php
@@ -0,0 +1,180 @@
+<div id="float_plate" class="row">
+	<div id="plate_vector_map" class="plate_vector_map">
+		missing plate_vector_map.svg
+	</div>
+	<div id="plate_overall_progress">
+		<div id="pop_label">
+			<span class="set1">Overall Progress</span>
+			<span class="set2">Actual</span>
+			<span class="set3">Planned</span>
+			<span class="set4">Variance</span>
+			<span class="set5">Trend</span>
+		</div>
+		<div id="pop_value">
+			<span class="set1">76%</span>
+			<span class="set2">78%</span>
+			<span class="set3">-2w</span>
+			<span class="set4"><i class="fa fa-arrow-up" aria-hidden="true"></i> UP</span>
+		</div>
+	</div>
+	<div id="plate_commercial">
+		<div id="pcom_label">
+			<span class="set1">Project Cost Update</span>
+			<span class="set2">Project Spend To Date</span>
+			<span class="set3">Awarded Package</span>
+			<span class="set4">PDP Reimbursables</span>
+			<span class="set5">WPCs Payment</span>
+			<span class="set6">Retention Sum</span>
+			<span class="set7">Variation Orders</span>
+			<span class="set8">Contigency Sum</span>
+		</div>
+		<div id="pcom_value">
+			<span class="set1">16.75 Bil</span>
+			<span class="set2">21.52 Bil</span>
+			<span class="set3">1.55 Mil</span>
+			<span class="set4">13.50 Bil</span>
+			<span class="set5">382.51 Mil</span>
+			<span class="set6">502.80 Mil</span>
+			<span class="set7">775.46 Mil</span>
+		</div>
+	</div>
+	<div id="plate_legend">
+		<div id="pl_label_header">
+			<span class="set1">Legends</span>
+		</div>
+		<div id="pl_label_content">
+			<span class="set1">Not Started</span>
+			<span class="set2">On Schedule</span>
+			<span class="set3">Behind Late</span>
+			<span class="set4">Critical</span>
+		</div>
+	</div>
+	<!-- <div id="plate_issue">
+		<div id="pi_title">
+			<span class="set1"><i class="fa fa-exclamation-triangle" aria-hidden="true"></i></span>
+			<span class="set2">Issues</span>
+		</div>
+		<div id="pi_subtitle">
+			<span class="set1">Click to view</span>
+		</div>
+	</div> -->
+			
+			
+			
+	<div id="plate_system">
+		<div id="donut_plate">
+			<div id="dp_db_donut_0" class="dp_top"></div>
+			<div id="dp_db_donut_1" class="dp_bottom"></div>
+			<div id="dp_db_donut_2" class="dp_bottom"></div>
+			<div id="dp_db_donut_3" class="dp_bottom"></div>
+			<div id="dp_db_donut_4" class="dp_bottom"></div>
+			<div id="dp_db_donut_5" class="dp_bottom"></div>
+			<div id="dp_db_donut_6" class="dp_bottom"></div>
+			<div id="dp_db_donut_7" class="dp_bottom"></div>
+		</div>
+	</div>
+	
+	<div id="plate_physical">
+		<div id="pp_station">
+			<span class="set1"></span>
+		</div>
+		<div id="pp_depot">
+			<span class="set1"></span>
+		</div>
+	</div>
+	<div id="plate_label">
+		<div id="pl_depot">
+			<span class="set1">Serdang Depot</span>
+		</div>
+		<div id="pl_station">
+			<span class="set1">Sg. Buloh</span>
+			<span class="set2">D'sara Damai</span>
+			<span class="set3">Sri D'sara West</span>
+			<span class="set4">Sri D'sara East</span>
+			<span class="set5">Kepong Sentral</span>
+			<span class="set6">Metro Prima</span>
+			<span class="set7">Kepong Baru</span>
+			<span class="set8">Jinjang</span>
+			<span class="set9">Sri Delima</span>
+			<span class="set10">Kg. Batu</span>
+			<span class="set11">Kentonmen</span>
+			<span class="set12">Jln. Ipoh</span>
+			<span class="set13">Sentul West</span>
+			<span class="set14">Titiwangsa</span>
+			<span class="set15">HKL</span>
+			<span class="set16">Kg. Baru North</span>
+			<span class="set17">Ampang Park</span>
+			<span class="set18">KLCC East</span>
+			<span class="set19">Conlay</span>
+			<span class="set20">TRX</span>
+			<span class="set21">Chan Sow Lin</span>
+			<span class="set22">Bdr. Malaysia North</span>
+			<span class="set23">Bdr. Malaysia South</span>
+			<span class="set24">Kuchai Lama</span>
+			<span class="set25">Tmn. Naga Emas</span>
+			<span class="set26">Sg. Besi</span>
+			<span class="set27">Tech. Park</span>
+			<span class="set28">Serdang Raya North</span>
+			<span class="set29">Serdang Raya South</span>
+			<span class="set30">Sri Kembangan</span>
+			<span class="set31">UPM</span>
+			<span class="set32">Tmn. Universiti</span>
+			<span class="set33">Equine Park</span>
+			<span class="set34">Tmn. Putra Permai</span>
+			<span class="set35">16 Sierra</span>
+			<span class="set36">Cyberjaya North</span>
+			<span class="set37">Cyberjaya City Centre</span>
+			<span class="set38">Putrajaya Sentral</span>
+		</div>
+		<div id="pl_mspr">
+			<span class="set1">P</span>
+			<span class="set2">P</span>
+			<span class="set3">P</span>
+			<span class="set4">P</span>
+			<span class="set5">P</span>
+			<span class="set6">P</span>
+			<span class="set7">P</span>
+			<span class="set8">P</span>
+			<span class="set9">P</span>
+			<span class="set10">P</span>
+			<span class="set11">P</span>
+			<span class="set12">P</span>
+			<span class="set13">P</span>
+			<span class="set14">P</span>
+			<span class="set15">P</span>
+		</div>
+<!--		Commented by Sebin-->
+<!--		<div id="pl_package">-->
+<!--			<span id="pl_package_V201" class="set1" style="cursor:pointer">V201</span>-->
+<!--			<span class="set2" onclick="location.href='/mpxd2/v2/index'" style="cursor:pointer">V202</span>-->
+<!--			<span class="set3" onclick="location.href='/mpxd2/v3/index'" style="cursor:pointer">V203</span>-->
+<!--			<span class="set4">UG</span>-->
+<!--			<span class="set5">V204</span>-->
+<!--			<span class="set6">V205</span>-->
+<!--			<span class="set7">V206</span>-->
+<!--			<span class="set8">V207</span>-->
+<!--			<span class="set9">V208</span>-->
+<!--			<span class="set10">V209</span>-->
+<!--			<span class="set11" onclick="location.href='/mpxd2/v10/index'" style="cursor:pointer">V210</span>-->
+<!--		</div>-->
+
+<!--		Added by Sebin-->
+		<div id="pl_package">
+			<a class="set1" href="<?php echo $this->config->base_url(); ?>v201/index">V201</a>
+			<a class="set2" href="<?php echo $this->config->base_url(); ?>v202/index">V202</a>
+			<a class="set3" href="<?php echo $this->config->base_url(); ?>v203/index">V203</a>
+			<a class="set4">UG</a>
+			<a class="set5" href="<?php echo $this->config->base_url(); ?>v204/index">V204</a>
+			<a class="set6" href="<?php echo $this->config->base_url(); ?>v205/index">V205</a>
+			<a class="set7" href="<?php echo $this->config->base_url(); ?>v206/index">V206</a>
+			<a class="set8" href="<?php echo $this->config->base_url(); ?>v207/index">V207</a>
+			<a class="set9" href="<?php echo $this->config->base_url(); ?>v208/index">V208</a>
+			<a class="set10" href="<?php echo $this->config->base_url(); ?>v209/index">V209</a>
+			<a class="set11" href="<?php echo $this->config->base_url(); ?>v210/index">V210</a>
+		</div>
+		<div id="pl_phase">
+			<span class="set1">NORTH</span>
+			<span class="set2">SOUTH</span>
+		</div>
+	</div>
+</div>
