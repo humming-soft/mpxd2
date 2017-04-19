caecd511723d6233302a9dbbc241e12667fbabf3
unknown <Sebin@HS-PC07.Office.Hummingsoft.com.my>
2017-04-19 12:00:03 +0800

    Signed-off-by: unknown <Sebin@HS-PC07.Office.Hummingsoft.com.my>

--- assets/js/mpxd.js
+++ assets/js/mpxd.js
@@ -3,8 +3,9 @@ mpxd = {};
 
 isPDF = true;
 isUseCustomPortlet = (getUrlParameter("print") == "1");
+var imageBase = window.location.origin;
 // mpxd.siteurl = 'https://mpxd.mymrt.com.my/';
-mpxd.siteurl = 'http://192.168.1.63:90/images/';
+mpxd.siteurl = imageBase+'/images/';
 $(function(){
 	/* Optimize page for print layout */
 
